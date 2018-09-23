fac15203dacc805e99aaed8d4e3431766f0d4013
Michael Dorst <mikefdorst@mikefdorst.com>
2018-09-23 11:05:26 -0700

    Add specification for SizeException

--- src/grading/SizeException.java
+++ src/grading/SizeException.java
@@ -1,5 +1,9 @@
 package grading;
 
+/*
+ * [X] 1. The `SizeException` class must be a checked exception.
+ * Note: this means it must extend `Exception`, and not `RuntimeException`.
+ */
 public class SizeException extends Exception
 {
     public final long serialVersionUID = 1L;
