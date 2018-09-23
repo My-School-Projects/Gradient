b3be43a3296885f1a48d759199a35636c8350cd7
Michael Dorst <mikefdorst@mikefdorst.com>
2018-09-23 11:13:18 -0700

    Add specification for TotalStrategy

--- src/grading/TotalStrategy.java
+++ src/grading/TotalStrategy.java
@@ -2,6 +2,22 @@ package grading;
 
 import java.util.List;
 
+/*
+ * [ ] 1. `public` methods must not have any side effects. That is, they must not change the parameters that
+ * they are passed in any way (e.g., the `List` that is passed to the `calculate()` method must not be changed
+ * in any way) and they must not change attributes that are not "owned" (i.e., attributes that are aliases) in
+ * any way.
+ *
+ * [ ] 2. The `calculate()` method must calculate the total of the `List` of `Grade` objects it is passed.
+ *      [ ] 2.1. You may assume that the `calculate()` method is passed a `List` that does not contain any
+ *      `null` elements.
+ *      [ ] 2.2. If the `List` is `null` then it must throw a `SizeException`.
+ *      [ ] 2.3. If the `List` is empty then it must throw a `SizeException`.
+ *      [ ] 2.4. Otherwise, it must return a `Grade` object with the given key and a value equal to the total
+ *      of the `Grade` objects in the `List`.
+ *          [ ] 2.4.1. If the value of a particular `Grade` is missing (i.e., `null`) then the value of `0.0`
+ *          must be used. Note: The `Missing` class has a method that can be sued to accomplish this.
+ */
 public class TotalStrategy implements GradingStrategy
 {
 
